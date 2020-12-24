d14e595eb16e7b9673fb54c11acf221ed3ed6af5
JeffB42 <10328858+JeffB42@users.noreply.github.com>
2020-12-23 21:19:43 -0800

    changed version number to 2.6.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_zbolt_octoscreen"
 plugin_name = "OctoPrint-Z-Bolt-OctoScreen"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "1.2.0"
+plugin_version = "2.6.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
